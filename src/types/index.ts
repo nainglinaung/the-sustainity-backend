3fbf944a97faa452bb0ee0795f07341745d3d86f
nainglinaung <nainglinaung91@gmail.com>
2025-02-18 15:42:14 +0700

    chore: refactor

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,7 +3,11 @@ interface MappingDefinition {
     desiredName: string;
     desiredType: 'string' | 'number' | 'date' | 'boolean';
 }
-  
+
+export interface PreviewRequest {
+    fileName: string;
+}
+
 export interface ProcessRequest {
     mappingName: string;
     fileName: string;
@@ -13,6 +17,26 @@ export interface SaveMappingRequest {
     mappings: MappingDefinition[];
     name: string;
 }
+
+
+export interface ValidationError {
+    row: number;
+    field: string;
+    error: string;
+    value: string;
+}
+
+export interface PaginatedResponse {
+    data: Record<string, any>[];
+    pagination: {
+      total: number;
+      page: number;
+      limit: number;
+      totalPages: number;
+    };
+}
+  
+
 
 
 export interface ValidationError {
